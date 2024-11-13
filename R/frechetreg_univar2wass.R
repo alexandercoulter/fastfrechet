c16f5427d5e53064925d052c658ad2d9470f1756
llrebecca21 <llrebecca21@gmail.com>
2024-11-13 13:08:14 -0600

    Remove skip checks as an argument.Remove the boolean check for skip checks in the function.

--- R/frechetreg_univar2wass.R
+++ R/frechetreg_univar2wass.R
@@ -1,5 +1,5 @@
 #' (Global) Fréchet Regression for Univariate Distributions with 2-Wasserstein Metric
-#' 
+#'
 #' @description
 #' `frechetreg_univar2wass()` calculates global Fréchet regression mean
 #' objects \insertCite{petersen_frechet_2019}{fastfrechet},
@@ -11,7 +11,7 @@
 #' and box constraints. The Fréchet regression problem in this context is reducible
 #' to a quadratic programming problem; the workhorse of this function is a custom
 #' active set method based on \insertCite{arnstrom_dual_2022}{fastfrechet}.
-#' 
+#'
 #'
 #' @param X A (`n` \eqn{\times} `p`) "input" covariate matrix with no missing, all finite entries.
 #' @param Y A (`n` \eqn{\times} `m`) matrix of observed quantile functions, row-wise monotone non-decreasing.
@@ -26,313 +26,266 @@
 #'
 #' @return A (`n` \eqn{\times} `m`) matrix that is the unique solution to the Fréchet Regression problem for univariate distribution responses.
 #'  The solution has rows that are monotone decreasing that are bounded between the `lower` and `upper` arguments.
-#' 
-#' @references 
+#'
+#' @references
 #' \insertRef{arnstrom_dual_2022}{fastfrechet}
-#' 
+#'
 #' \insertRef{petersen_frechet_2019}{fastfrechet}
-#' 
+#'
 #' \insertRef{tucker_variable_2023}{fastfrechet}
-#' 
+#'
 #' @export
 #'
 #' @examples
-frechetreg_univar2wass = function(X,
-                                  Y,
-                                  Z = NULL,
-                                  lambda = NULL,
-                                  lower = -Inf,
-                                  upper = Inf,
-                                  eps = 1e-10,
-                                  skip_checks = FALSE){
-  
+frechetreg_univar2wass <- function(X,
+                                   Y,
+                                   Z = NULL,
+                                   lambda = NULL,
+                                   lower = -Inf,
+                                   upper = Inf,
+                                   eps = 1e-10) {
   # Compatibility and dimension checks:
-  if(!skip_checks){
-    
-    # Check for matrix inputs (X and Y; Z if provided):
-    if(!(is.matrix(X) & is.matrix(Y))) stop('Y and X should be matrices.')
-    if(!is.null(Z)) if(!is.matrix(Z)) stop('Z should be a matrix.')
-    
-    # Check for row matching between X and Y:
-    if(nrow(X) != nrow(Y)) stop('Y and X should have the same number of rows.')
-    
-    # Check for length and non-negativity of sparsity vector, if provided:
-    if(!is.null(lambda)) if(ncol(X) != length(lambda)) stop('lambda should have the same length as X has rows.')
-    if(!is.null(lambda)) if(any(lambda < 0)) stop('All lambda entries must be non-negative.')
-    
-    # Check for box constraint compatibility:
-    if(lower >= upper) stop('Lower bound should be strictly less than upper bound.')
-    
-    # Check error tolerance is strictly positive:
-    if(eps <= 0) stop('Error tolerance should be strictly positive.')
-    
-    # Check for column matching between X and Z, if provided:
-    if(!is.null(Z)) if(ncol(X) != ncol(Z)) stop('X and Z must have the same number of columns.')
-    
-    # Check for monotonicity of Y values:
-    if(ncol(Y) > 1) if(min(Y[ , -1] - Y[ , -ncol(Y)]) < 0) stop('Y should be row-wise monotone.')
-    
-    # Check for box constraints on Y values:
-    if((max(Y) > upper) | (min(Y) < lower)) stop('Y should obey box constraints.')
-    
-  }
-  
+
+  # Check for matrix inputs (X and Y; Z if provided):
+  if (!(is.matrix(X) & is.matrix(Y))) stop("Y and X should be matrices.")
+  if (!is.null(Z)) if (!is.matrix(Z)) stop("Z should be a matrix.")
+
+  # Check for row matching between X and Y:
+  if (nrow(X) != nrow(Y)) stop("Y and X should have the same number of rows.")
+
+  # Check for length and non-negativity of sparsity vector, if provided:
+  if (!is.null(lambda)) if (ncol(X) != length(lambda)) stop("lambda should have the same length as X has rows.")
+  if (!is.null(lambda)) if (any(lambda < 0)) stop("All lambda entries must be non-negative.")
+
+  # Check for box constraint compatibility:
+  if (lower >= upper) stop("Lower bound should be strictly less than upper bound.")
+
+  # Check error tolerance is strictly positive:
+  if (eps <= 0) stop("Error tolerance should be strictly positive.")
+
+  # Check for column matching between X and Z, if provided:
+  if (!is.null(Z)) if (ncol(X) != ncol(Z)) stop("X and Z must have the same number of columns.")
+
+  # Check for monotonicity of Y values:
+  if (ncol(Y) > 1) if (min(Y[, -1] - Y[, -ncol(Y)]) < 0) stop("Y should be row-wise monotone.")
+
+  # Check for box constraints on Y values:
+  if ((max(Y) > upper) | (min(Y) < lower)) stop("Y should obey box constraints.")
+
   # Solve for Yhat (i.e. unconstrained weighted mean):
   {
-    
     # Check for presence of Z, the "output matrix":
-    if(!is.null(Z)){
-      
+    if (!is.null(Z)) {
       # Center and scale X, Z matrices:
-      output = scaleXZ_cpp(X, Z, tol = 1e-10);
-      Xc = output$Xc
-      Zc = output$Zc
-      
+      output <- scaleXZ_cpp(X, Z, tol = 1e-10)
+      Xc <- output$Xc
+      Zc <- output$Zc
+
       # Grab dimensions:
-      n = nrow(Xc)
-      p = ncol(Xc)
-      nz = nrow(Zc)
-      
+      n <- nrow(Xc)
+      p <- ncol(Xc)
+      nz <- nrow(Zc)
+
       # If lambda is not present (i.e. non-regularized regression)...
-      if(is.null(lambda)){
-        
+      if (is.null(lambda)) {
         # For simplicity in comments, let X = Xc. The following  evaluates
-        # 
+        #
         # ( 1/n J + Z( X'X )^{+}X' )Y
-        # 
+        #
         # which requires either inverting X'X (if possible) or finding the
         # right singular vectors of X. In case X'X is not invertible, the
         # SVD of X or the SVD of X'X (both through which the right singular
         # vectors can be found) should be selected by comparing n vs. p.
-        # 
+        #
         # We re-write the expression of interest as
-        # 
+        #
         # ( 1/n J + ZM )Y,
-        # 
+        #
         # and try first to invert X'X. If it fails, we take an SVD route.
-        if(p > (0.9 * n)){
-          
+        if (p > (0.9 * n)) {
           # In case p out-scales n, SVD on X is faster than SVD on X'X:
-          M = tryCatch(solve(crossprod(Xc), crossprod(Xc, Y)),
-                       error = function(e){
-                         
-                         S = svd(Xc)
-                         g = S$d > 1e-10
-                         return(list(S$v[ , g, drop = FALSE], crossprod(S$v[ , g, drop = FALSE], crossprod(Xc, Y)) / (S$d[g]^2)))
+          M <- tryCatch(solve(crossprod(Xc), crossprod(Xc, Y)),
+            error = function(e) {
+              S <- svd(Xc)
+              g <- S$d > 1e-10
+              return(list(S$v[, g, drop = FALSE], crossprod(S$v[, g, drop = FALSE], crossprod(Xc, Y)) / (S$d[g]^2)))
+            }
+          )
 
-                       })
-          
-          if(!is.list(M)){
-            
+          if (!is.list(M)) {
             # Calculate Yhat:
-            Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% M
-            
+            Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% M
           } else {
-            
             # Calculate Yhat, preferring to first multiply Z by the right
             # singular vector matrix V[ , g] under the assumption n ~ nz:
-            Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + (Zc %*% M[[1]]) %*% M[[2]]
-            
+            Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + (Zc %*% M[[1]]) %*% M[[2]]
           }
-          
         } else {
-          
           # In case n out-scales p, SVD on X'X is faster than SVD on X:
-          M = tryCatch(solve(crossprod(Xc), crossprod(Xc, Y)),
-                       error = function(e){
-                         
-                         S = svd(crossprod(Xc))
-                         g = S$d > 1e-10
-                         return(list(S$v[ , g, drop = FALSE], crossprod(S$v[ , g, drop = FALSE], crossprod(Xc, Y)) / (S$d[g])))
-                         
-                       })
-          
-          if(!is.list(M)){
-            
+          M <- tryCatch(solve(crossprod(Xc), crossprod(Xc, Y)),
+            error = function(e) {
+              S <- svd(crossprod(Xc))
+              g <- S$d > 1e-10
+              return(list(S$v[, g, drop = FALSE], crossprod(S$v[, g, drop = FALSE], crossprod(Xc, Y)) / (S$d[g])))
+            }
+          )
+
+          if (!is.list(M)) {
             # Calculate Yhat:
-            Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% M
-            
+            Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% M
           } else {
-            
             # Calculate Yhat, preferring to first multiply the matrices from
             # M's output under the assumption n ~ nz:
-            Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% (M[[1]] %*% M[[2]])
-            
+            Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zc %*% (M[[1]] %*% M[[2]])
           }
-
         }
-        
       } else {
-        
         # Scale Xc and Zc by sqrt(n) to simplify algebra with ridge penalty:
-        Xcn = Xc / sqrt(n)
-        Zcn = Zc / sqrt(n)
-        
+        Xcn <- Xc / sqrt(n)
+        Zcn <- Zc / sqrt(n)
+
         # For simplicity in comments, let X = Xcn and Z = Zcn. The following
         # evaluates
-        # 
+        #
         # ( 1/n J + Z( X'X + D^{-1} )^{-1}X' )Y
-        # 
+        #
         # which requires inverting a matrix. When p is large in comparison to
         # n, we can rewrite the above expression
-        # 
+        #
         # ( 1/n J + ZDX'(XDX' + I)^{-1} )Y
-        # 
+        #
         # which involves inverting an (n x n) matrix. When n is large, we use
-        # 
+        #
         # ( 1/n J + ZB(BX'XB + I)^{-1}BX' )Y
-        # 
+        #
         # where B = D^{1/2}, which involves inverting a (p x p) matrix.
-        if(p > (1.1 * n)){
-          
+        if (p > (1.1 * n)) {
           # Calculate DX':
-          DX = t(Xcn) * lambda
-          
+          DX <- t(Xcn) * lambda
+
           # Calculate XDX' + I:
-          G = Xcn %*% DX
-          diag(G) = diag(G) + 1
-          
+          G <- Xcn %*% DX
+          diag(G) <- diag(G) + 1
+
           # Calculate Yhat:
-          Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + (Zcn %*% DX) %*% solve(G, Y)
-          
+          Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + (Zcn %*% DX) %*% solve(G, Y)
         } else {
-          
           # Find b = sqrt(d), implicitly giving B = D^{1/2}:
-          root_lambda = sqrt(lambda)
-          
+          root_lambda <- sqrt(lambda)
+
           # Calculate BX'XB + I:
-          G = crossprod(Xcn) * tcrossprod(root_lambda)
-          diag(G) = diag(G) + 1
-          
+          G <- crossprod(Xcn) * tcrossprod(root_lambda)
+          diag(G) <- diag(G) + 1
+
           # Calculate Yhat:
-          Yhat = rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zcn %*% (root_lambda * solve(G, root_lambda * crossprod(Xcn, Y)))
-          
+          Yhat <- rep(1, nz) %*% crossprod(rep(1 / n, n), Y) + Zcn %*% (root_lambda * solve(G, root_lambda * crossprod(Xcn, Y)))
         }
-        
       }
-      
     } else {
-      
       # Center and scale X matrix:
-      Xc = scaleX_cpp(X)
-      
+      Xc <- scaleX_cpp(X)
+
       # Grab dimensions:
-      n = nrow(Xc)
-      p = ncol(Xc)
-      m = ncol(Y)
-      
+      n <- nrow(Xc)
+      p <- ncol(Xc)
+      m <- ncol(Y)
+
       # If lambda is not present (i.e. non-regularized regression)...
-      if(is.null(lambda)){
-        
+      if (is.null(lambda)) {
         # For simplicity in comments, let X = Xc. The following  evaluates
-        # 
+        #
         # ( 1/n J + X( X'X )^{+}X' )Y
-        # 
+        #
         # i.e. the projection of Y onto ColSpace(1, X). This requires either
         # inverting X'X (if possible) or finding the right singular vectors of
         # X.
-        # 
+        #
         # We re-write the expression of interest as
-        # 
+        #
         # ( 1/n J + ZM )Y,
-        # 
+        #
         # and compare p vs. n. If p is at least as large as n, we immediately
         # skip to the SVD method. If it is less than n, we try to invert X'X,
         # and if that fails then we do the SVD method.
-        if(p >= n){
-          
+        if (p >= n) {
           # Immediately calculate SVD of Xc = USV':
-          S = svd(Xc)
-          g = S$d > 1e-10
-          
+          S <- svd(Xc)
+          g <- S$d > 1e-10
+
           # Evaluate UU'Y, ordering the multiplication based on n > m:
-          M = if(n > m) S$u[ , g, drop = FALSE] %*% crossprod(S$u[ , g, drop = FALSE], Y) else tcrossprod(S$u[ , g, drop = FALSE]) %*% Y
-          
+          M <- if (n > m) S$u[, g, drop = FALSE] %*% crossprod(S$u[, g, drop = FALSE], Y) else tcrossprod(S$u[, g, drop = FALSE]) %*% Y
+
           # Calculate Yhat:
-          Yhat = rep(1, n) %*% crossprod(rep(1 / n, n), Y) + M
-          
+          Yhat <- rep(1, n) %*% crossprod(rep(1 / n, n), Y) + M
         } else {
-          
           # Try to solve X(X'X)^{-1}X'Y through inversion method:
-          M = tryCatch(Xc %*% solve(crossprod(Xc), crossprod(Xc, Y)),
-                       error = function(e){
-                         
-                         # If inversion fails, calculate SVD of Xc = USV':
-                         S = svd(Xc)
-                         g = S$d > 1e-10
-                         
-                         # Evaluate UU'Y, ordering the multiplication based on n > m:
-                         if(n > m) S$u[ , g, drop = FALSE] %*% crossprod(S$u[ , g, drop = FALSE], Y) else tcrossprod(S$u[ , g, drop = FALSE]) %*% Y
-                         
-                       })
-          
+          M <- tryCatch(Xc %*% solve(crossprod(Xc), crossprod(Xc, Y)),
+            error = function(e) {
+              # If inversion fails, calculate SVD of Xc = USV':
+              S <- svd(Xc)
+              g <- S$d > 1e-10
+
+              # Evaluate UU'Y, ordering the multiplication based on n > m:
+              if (n > m) S$u[, g, drop = FALSE] %*% crossprod(S$u[, g, drop = FALSE], Y) else tcrossprod(S$u[, g, drop = FALSE]) %*% Y
+            }
+          )
+
           # Calculate Yhat:
-          Yhat = rep(1, n) %*% crossprod(rep(1 / n, n), Y) + M
-        
+          Yhat <- rep(1, n) %*% crossprod(rep(1 / n, n), Y) + M
         }
-        
       } else {
-        
         # Scale Xc sqrt(n) to simplify algebra with ridge penalty:
-        Xcn = Xc / sqrt(n)
-        
+        Xcn <- Xc / sqrt(n)
+
         # For simplicity in comments, let X = Xcn. The following evaluates
-        # 
+        #
         # ( 1/n J + X( X'X + D^{-1} )^{+}X' )Y
-        # 
+        #
         # which can be rewritten based on the size of n vs. p. If p is larger
         # than n, a helpful equivalent representation is
-        # 
+        #
         # ( 1/n J + I - (XDX' + I)^{-1} )Y
-        # 
+        #
         # which involves inverting an (n x n) matrix. If n is larger than p,
         # a helpful equivalent representation is
-        # 
+        #
         # ( 1/n J + X(DX'X + I)^{-1}DX' )Y
-        # 
+        #
         # which involves inverting a (p x p) matrix.
-        if(p > n){
-          
+        if (p > n) {
           # Calculate XDX' + I:
-          G = Xcn %*% (lambda * t(Xcn))
-          diag(G) = diag(G) + 1
-          
+          G <- Xcn %*% (lambda * t(Xcn))
+          diag(G) <- diag(G) + 1
+
           # Calculate Yhat:
-          Yhat = rep(1, n) %*% crossprod(rep(1 / n, n), Y) + Y - solve(G, Y)
-          
+          Yhat <- rep(1, n) %*% crossprod(rep(1 / n, n), Y) + Y - solve(G, Y)
         } else {
-          
           # Calculate DX'X + I:
-          S = crossprod(Xcn)
-          G = lambda * S
-          diag(G) = diag(G) + 1
-          
+          S <- crossprod(Xcn)
+          G <- lambda * S
+          diag(G) <- diag(G) + 1
+
           # Calculate Yhat:
-          Yhat = rep(1, n) %*% crossprod(rep(1 / n, n), Y) + Xcn %*% solve(G, lambda * crossprod(Xcn, Y))
-          
+          Yhat <- rep(1, n) %*% crossprod(rep(1 / n, n), Y) + Xcn %*% solve(G, lambda * crossprod(Xcn, Y))
         }
-        
       }
-      
     }
-    
   }
-  
+
   # Solve for Qhat (i.e. constrained weighted mean):
   {
-    
     # Lagrange multiplier from custom active set method:
-    Eta = Custom_Active_Set(Y = Yhat,
-                            L = cbind(rep(lower, nrow(Yhat))),
-                            U = cbind(rep(upper, nrow(Yhat))),
-                            eps = eps)
-    
+    Eta <- Custom_Active_Set(
+      Y = Yhat,
+      L = cbind(rep(lower, nrow(Yhat))),
+      U = cbind(rep(upper, nrow(Yhat))),
+      eps = eps
+    )
+
     # Calculate Qhat from stability optimality condition:
-    Qhat = Yhat + (Eta[ , -ncol(Eta)] - Eta[ , -1])
-    
+    Qhat <- Yhat + (Eta[, -ncol(Eta)] - Eta[, -1])
   }
-  
+
   # Return Qhat value:
   return(Qhat)
-  
 }

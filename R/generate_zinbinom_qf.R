6fd6fe5e00a565708f38d39bb273bc2db126bb2d
alexandercoulter <coultera@tamu.edu>
2024-11-14 16:50:06 -0600

    Correct error in new bounds for parameters.

--- R/generate_zinbinom_qf.R
+++ R/generate_zinbinom_qf.R
@@ -57,7 +57,7 @@ generate_zinbinom_qf = function(n,
   check_numeric(size, "scalar", finite = TRUE)
   # if(!is.vector(zero_inflation) | !is.vector(prob) | !is.vector(size)) stop("'zero_inflation', 'prob', and 'size' must all be numeric scalars.")
   # if(length(zero_inflation) != 1 | length(prob) != 1 | length(size) != 1) stop("'zero_inflation', 'prob', and 'size' must all be numeric scalars.")
-  if((zero_inflation <= 0) | (zero_inflation >= 1)) stop("'zero_inflation' must be strictly in [0, 1].")
+  if((zero_inflation < 0) | (zero_inflation > 1)) stop("'zero_inflation' must be strictly in [0, 1].")
   if((prob <= 0) | (prob > 1)) stop("'prob' must be strictly in (0, 1].")
   if(size <= 0) stop("'size' must be strictly positive.")
   

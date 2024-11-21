dbfd2dc886cb75335eec31ab5e8b9b87e05e3568
alexandercoulter <coultera@tamu.edu>
2024-11-21 13:34:27 -0600

    Include C_init as optional parameter for monotoneQP; add checks and documentation for C_init.

--- R/monotoneQP.R
+++ R/monotoneQP.R
@@ -6,13 +6,16 @@
 #'  vector in Frobenius norm to an input vector, with the constraints that the 
 #'  output vector should be monotone non-decreasing across its entries,
 #'  and obey user-specified box constraints. The function permits a matrix input,
-#'  where this QP problem is solved row-wise. Implementation is based on the
-#'  dual-active set method of \insertCite{arnstrom_dual_2022}{fastfrechet},
-#'  taking advantage of simplifications in this setting which avoid LDL' 
-#'  decompositions and other costly matrix product operations.
+#'  where this QP problem is solved row-wise, and also permits warm starts, in
+#'  the form of specifying an initial active-constraint matrix. Implementation
+#'  is based on the dual-active set method of
+#'  \insertCite{arnstrom_dual_2022}{fastfrechet}, taking advantage of
+#'  simplifications in this setting which avoid \eqn{LDL^{\top}} decompositions
+#'  and other costly matrix product operations.
 #'  
 #' @inheritParams frechetreg_univar2wass
-#' @param Y A numeric vector or matrix.
+#' @param Y An (`m`)-long numeric vector (or (`n` \eqn{\times} `m`) matrix) with no missing, all finite entries.
+#' @param C_init An optional (`m + 1`)-long numeric vector (or (`n` \eqn{\times} `m + 1`) matrix), specifying initial active set(s) for optimization. Active sets are identified by positive entries, row-wise if a matrix.
 #' @param eps tolerance level (default `1e-10`).
 #' 
 #' @references 
@@ -28,6 +31,7 @@
 #'
 #' @examples
 monotoneQP = function(Y,
+                      C_init = NULL,
                       lower = -Inf,
                       upper = Inf,
                       eps = 1e-10){
@@ -42,6 +46,22 @@ monotoneQP = function(Y,
   # Check Y has at least one row and one column:
   if(prod(dim(Y)) == 0) stop("'Y' must have at least one row and column.")
   
+  # Numeric check and conversion for optional C_init:
+  if(!is.null(C_init)){
+    
+    if(is.vector(C_init)) C_init = rbind(C_init)
+    check_numeric(C_init, "matrix", FALSE)
+    if(min(C_init) < 0) stop("'C_init' must contain non-negative entries.")
+    if(nrow(C_init) != nrow(Y)) stop("'Y' and 'C_init' must have same number of rows.")
+    if(ncol(C_init) != (ncol(Y) + 1)) stop("'C_init' must have one more column than 'Y'.")
+    C_init = sign(C_init)
+    
+  } else {
+    
+    C_init = matrix(0, nrow(Y), ncol(Y) + 1)
+    
+  }
+  
   # Numeric scalar check and compatibility checks for lower/upper:
   check_numeric(lower, "scalar", finite = FALSE)
   check_numeric(upper, "scalar", finite = FALSE)
@@ -53,7 +73,7 @@ monotoneQP = function(Y,
   
   # Run custom active set method to obtain Lagrange multiplier 'Eta':
   Eta = monotoneQP_warmstart(Y = Y,
-                             W_init = matrix(0, nrow(Y), ncol(Y) + 1),
+                             C_init = C_init,
                              lower = lower,
                              upper = upper,
                              eps = eps)

598ad74edfc4dcd34f43550d8092b7f919cde49f
llrebecca21 <llrebecca21@gmail.com>
2024-11-21 18:46:44 -0600

    Update the example documentation.

--- R/FRiSO_univar2wass.R
+++ R/FRiSO_univar2wass.R
@@ -43,6 +43,29 @@
 #' @export
 #'
 #' @examples
+#' # Dense grid of 'allowance' totals
+#' tauseq = seq(20, 0.2, -0.2)
+#' # Generate estimated 'allowance' vectors \lambda for each \tau, stored
+#' # column-wise in matrix 'L'.
+#' L = fastfrechet::FRiSO_univar2wass(X = X,
+#'                                    Y = Y,
+#'                                    lower = 0,
+#'                                    upper = Inf,
+#'                                    tauseq = tauseq,
+#'                                    eps = 0.001,
+#'                                    nudge = 0.001)
+#'                                  
+#' # Plot FRiSO results in new plot:
+#' plot(x = c(), y = c(), xlim = c(0, max(tauseq)), ylim = c(0, max(L)),
+#'      main = 'FRiSO Selection Paths', xlab = expression(tau),
+#'      ylab = expression(lambda))
+#' for(k in 1:p) lines(tauseq, L[k, ], col = (k %in% 1:4) + 1, lwd = 2)
+#' legend('topleft', lwd = 2, col = 1:2, bty = 'n',
+#' legend = c('Unrelated variable', 'Model variable'))
+#' 
+#' # Note that with this sample size, we have sufficient information to correctly
+#' # identify the first 4 variables as significantly affecting our QFs.
+#' print(difftime(Sys.time(), t1))
 FRiSO_univar2wass = function(X,
                              Y,
                              lower = -Inf,

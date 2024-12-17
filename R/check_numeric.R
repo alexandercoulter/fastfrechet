7fcb13e1dca9aa88f250ba99818a20410134a6be
alexandercoulter <coultera@tamu.edu>
2024-12-17 08:57:36 -0500

    Reorganize parameters in roxygen skeleton, add a function decription, and add parameter descriptions.

--- R/check_numeric.R
+++ R/check_numeric.R
@@ -1,12 +1,18 @@
 #' Check if input is a numeric scalar, vector, or matrix, with no missing values
 #'
-#' @param x 
-#' @param finite 
-#' @param object_type 
+#' @description
+#' This function checks to see if `x` is a numeric object with non-missing or
+#' non-infinite entries. `x` may be a scalar, a vector, or a matrix. If `x` is
+#' one of these or has any missing or infinite (if specified) or non-numeric
+#' entries, the function stops the current R process and returns an error.
+#' 
+#' @param x Any R object.
+#' @param object_type A character object, one of `scalar`, `vector`, or `matrix` (default `scalar`), indicating which data value type is expected. Note, for this function, a `1`\eqn{\times}`1` matrix is not a scalar, and an `n`\eqn{\times}`1` matrix is not a vector.
+#' @param finite A logical `TRUE` or `FALSE` (default `TRUE`), indicating whether the function should check for `-Inf` or `Inf`.
 #'
 #' @return No returned value
 #'
-check_numeric = function(x, object_type = c("scalar", "vector", "matrix"), finite = TRUE){
+check_numeric = function(x, object_type = c("scalar", "vector", "matrix")[1], finite = TRUE){
   
   # Get variable name:
   name_x = deparse(substitute(x))

70983eb206aa1f8603b949949bceaa16021e5ec2
alexandercoulter <coultera@tamu.edu>
2025-02-07 07:02:43 -0600

    Add check for monotonicity of thresholds in relative model size by allowing non-monotone model size vector to be taken in.

--- R/test-Shah_Samworth_thresholds_works.R
+++ R/test-Shah_Samworth_thresholds_works.R
@@ -18,4 +18,23 @@ test_that("Shah and Samworth threshold is bounded and monotone in theta", {
   expect_true(bounded_check[1] > 0 & bounded_check[2] <= 1,
               sprintf("The Thresholds are not bounded in (0, 1]: min(thr) = %.5f, max(thr) = %.5f", bounded_check[1], bounded_check[2]))
   
+  # Set parameters (permuting q)
+  p <- 30
+  q <- sample(29)
+  B <- 50
+  E_thr <- 1
+  
+  # Calculate Shah and Samworth thresholds:
+  output <- Shah_Samworth_thresholds(p = p, q = q, B = B, E_thr = E_thr)
+  
+  # Check that given thresholds are monotone in relative model size q / p:
+  monotonicity_check <- round(min(diff(output$pointwise_thresholds[order(output$relative_model_size)])), 5)
+  expect_true(monotonicity_check >= 0,
+              sprintf("The Thresholds are not non-decreasing in theta: min(diff) = %.5f", monotonicity_check))
+  
+  # Check that Shah and Samworth thresholds are bounded in (0, 1]:
+  bounded_check <- range(output$pointwise_thresholds)
+  expect_true(bounded_check[1] > 0 & bounded_check[2] <= 1,
+              sprintf("The Thresholds are not bounded in (0, 1]: min(thr) = %.5f, max(thr) = %.5f", bounded_check[1], bounded_check[2]))
+  
 })

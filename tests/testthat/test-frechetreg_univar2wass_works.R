c05bd8a00a1f059f8b62835368a6e20374852a24
alexandercoulter <coultera@tamu.edu>
2025-02-07 09:21:54 -0600

    Adjust one test to remove explicit definition of Z; add test that includes different Z input.

--- tests/testthat/test-frechetreg_univar2wass_works.R
+++ tests/testthat/test-frechetreg_univar2wass_works.R
@@ -204,7 +204,61 @@ test_that("works with lambda", {
   # Estimate conditional QFs
   output <- frechetreg_univar2wass(X = X,
                                    Y = Y,
-                                   Z = X,
+                                   Z = NULL,
+                                   C_init = NULL,
+                                   lambda = lambda,
+                                   lower = lower,
+                                   upper = upper)
+  
+  # Extract predicted QFs
+  predicted_QFs <- output$Qhat  # Assuming this contains (n x m) matrix of QFs
+  
+  # Check monotonicity for each row (QFs should be non-decreasing)
+  apply(predicted_QFs, 1, function(row) {
+    expect_true(all(diff(row) >= -1e-5), info = "QF row is not monotonic (non-decreasing)")
+  })
+  
+  # Check lower bound for each element
+  expect_true(all(predicted_QFs >= lower - 1e-5), 
+              info = "QF values violate the lower bound")
+  
+  # Check upper bound for each element
+  expect_true(all(predicted_QFs <= upper + 1e-5), 
+              info = "QF values violate the upper bound of 40")
+})
+
+
+test_that("works with lambda and Z", {
+  # Parameters
+  lower <- 0   # Lower bound
+  upper <- 40  # Upper bound
+  
+  # Generate data
+  n <- 100
+  p <- 10
+  m <- 100
+  set.seed(31)
+  mydata <- generate_zinbinom_qf(n = n, p = p, m = m)
+  
+  X <- mydata$X
+  Y <- mydata$Y
+  
+  Y[Y > upper] <- upper
+  
+  # Generate random lambda vector with \tau sum
+  tau <- 10
+  g <- rnorm(p)
+  g <- g / sqrt(sum(g * g))
+  lambda <- g * g * tau
+  
+  # Generate Z matrix
+  nz = 10
+  Z <- matrix(rnorm(nz * p), nz, p)
+  
+  # Estimate conditional QFs
+  output <- frechetreg_univar2wass(X = X,
+                                   Y = Y,
+                                   Z = Z,
                                    C_init = NULL,
                                    lambda = lambda,
                                    lower = lower,

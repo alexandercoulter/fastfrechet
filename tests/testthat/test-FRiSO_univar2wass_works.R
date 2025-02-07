2cd76da15b99d164dc9567e9bf88b5fdc5d2076d
alexandercoulter <coultera@tamu.edu>
2025-02-07 09:48:42 -0600

    Create test file for FRiSO_univar2wass, with tests for large p setting and for gradient descent with momentum.

---
+++ tests/testthat/test-FRiSO_univar2wass_works.R
@@ -0,0 +1,149 @@
+test_that("works with sequence of tau's", {
+  # Parameters
+  lower <- 0   # Lower bound
+  upper <- 40  # Upper bound
+  
+  # Generate data
+  n <- 100
+  p <- 10
+  m <- 100
+  set.seed(31)
+  mydata <- generate_zinbinom_qf(n = n, p = p, m = m)
+  
+  X <- mydata$X
+  Y <- mydata$Y
+  
+  Y[Y > upper] <- upper
+  
+  tauseq = 1:10
+  formals(FRiSO_univar2wass)
+  # Calculate lambda-hat values
+  output <- FRiSO_univar2wass(X = X,
+                              Y = Y,
+                              lower = lower,
+                              upper = upper,
+                              tauseq = tauseq,
+                              lambda_init = NULL,
+                              eps = 1e-5,
+                              nudge = 0.001,
+                              alpha = 0.9,
+                              max_iter = 1000,
+                              max_theta = pi / 4,
+                              impulse = 1)
+  
+  # Check all lambda values are non-negative:
+  expect_true(all(output >= 0), 
+              info = "lambda values violate non-negativity")
+  
+  # Check lambda vectors add to tauseq values:
+  expect_true(all(max(abs(colSums(output) - tauseq)) <= 1e-10), 
+              info = "lambda values do not add to tau")
+})
+
+
+test_that("works with sequence of tau's and large p", {
+  # Parameters
+  lower <- 0   # Lower bound
+  upper <- 40  # Upper bound
+  
+  # Generate data
+  n <- 50
+  p <- 70
+  m <- 30
+  set.seed(31)
+  mydata <- generate_zinbinom_qf(n = n, p = p, m = m)
+  
+  X <- mydata$X
+  Y <- mydata$Y
+  
+  Y[Y > upper] <- upper
+  
+  tauseq = 1:10
+  formals(FRiSO_univar2wass)
+  # Calculate lambda-hat values
+  output <- FRiSO_univar2wass(X = X,
+                              Y = Y,
+                              lower = lower,
+                              upper = upper,
+                              tauseq = tauseq,
+                              lambda_init = NULL,
+                              eps = 1e-5,
+                              nudge = 0.001,
+                              alpha = 0.9,
+                              max_iter = 1000,
+                              max_theta = pi / 4,
+                              impulse = 1)
+  
+  # Check all lambda values are non-negative:
+  expect_true(all(output >= 0), 
+              info = "lambda values violate non-negativity")
+  
+  # Check lambda vectors add to tauseq values:
+  expect_true(all(max(abs(colSums(output) - tauseq)) <= 1e-10), 
+              info = "lambda values do not add to tau")
+})
+
+
+test_that("works with sequence of tau's and momentum (impulse < 1)", {
+  # Parameters
+  lower <- 0   # Lower bound
+  upper <- 40  # Upper bound
+  
+  # Generate data
+  n <- 100
+  p <- 10
+  m <- 100
+  set.seed(31)
+  mydata <- generate_zinbinom_qf(n = n, p = p, m = m)
+  
+  X <- mydata$X
+  Y <- mydata$Y
+  
+  Y[Y > upper] <- upper
+  
+  tauseq = 1:10
+  formals(FRiSO_univar2wass)
+  
+  # Calculate lambda-hat values with momentum
+  output1 <- FRiSO_univar2wass(X = X,
+                               Y = Y,
+                               lower = lower,
+                               upper = upper,
+                               tauseq = tauseq,
+                               lambda_init = NULL,
+                               eps = 1e-6,
+                               nudge = 0.001,
+                               alpha = 0.9,
+                               max_iter = 1000,
+                               max_theta = pi / 4,
+                               impulse = 0.8)
+  # Calculate lambda-hat values without momentum
+  output2 <- FRiSO_univar2wass(X = X,
+                               Y = Y,
+                               lower = lower,
+                               upper = upper,
+                               tauseq = tauseq,
+                               lambda_init = NULL,
+                               eps = 1e-6,
+                               nudge = 0.001,
+                               alpha = 0.9,
+                               max_iter = 1000,
+                               max_theta = pi / 4,
+                               impulse = 1)
+  
+  # Check all lambda values are non-negative:
+  expect_true(all(output1 >= 0), 
+              info = "lambda values violate non-negativity")
+  
+  # Check lambda vectors add to tauseq values:
+  expect_true(all(max(abs(colSums(output1) - tauseq)) <= 1e-10), 
+              info = "lambda values do not add to tau")
+  
+  # Check fitted lambda values are approximately equal:
+  expect_true(all(output1 >= 0), 
+              info = "lambda values violate non-negativity")
+  
+  # Check lambda vectors add to tauseq values:
+  expect_true(max(abs(output1 - output2)) <= 1e-5, 
+              info = "lambda values do not add to tau")
+})

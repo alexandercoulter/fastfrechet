a8b8398f3fa8e16e646f329d0da8482dbdd02616
alexandercoulter <coultera@tamu.edu>
2025-02-08 13:57:55 -0600

    Adjust script to account for private GitHub repo installation.

--- scripts/rangeL.R
+++ scripts/rangeL.R
@@ -1,10 +1,16 @@
+# Authenticate and install package if not installed
+if (!requireNamespace("fastfrechet", quietly = TRUE)) {
+  remotes::install_github("alexandercoulter/fastfrechet", auth_token = Sys.getenv("GITHUB_PAT"))
+}
+library(fastfrechet)
+
 n <- 200 # number of samples - nrow(X) and nrow(Y).
 p <- 20 # number of covariates - ncol(X).
 m <- 200 # quantile function grid density - ncol(Y).
 mseq <- seq(1 / (2 * m), 1 - 1 / (2 * m), length.out = m)
 
 set.seed(31)
-mydata <- fastfrechet::generate_zinbinom_qf(
+mydata <- generate_zinbinom_qf(
   n = n,
   p = p,
   m = m
